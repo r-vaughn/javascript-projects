2474e37d41fe58c86813145bfba170bbe510b21d
r-vaughn <rebecca.a.vaughn@outlook.com>
2024-01-21 22:23:53 -0600

    Orbit achieved

--- loops/exercises/while-Loop-Exercises.js
+++ loops/exercises/while-Loop-Exercises.js
@@ -1,25 +1,41 @@
 //Define three variables for the LaunchCode shuttle - one for the starting fuel level, another for the number of astronauts aboard, and the third for the altitude the shuttle reaches.
 
+const input = require('readline-sync')
 
+let fuelStart = 0
+let numAstronauts = 0 
+let shuttleAlt = 0
 
+//Exercise #4: Construct while loops to do the following:
+  //a. Query the user for the starting fuel level. Validate that the user enters a positive, integer value greater than 5000 but less than 30000.
 
+while (fuelStart <= 5000 || fuelStart >30000 || isNaN(fuelStart)) {
+    fuelStart = input.question("Enter the starting fuel level: ");
+}
 
-/*Exercise #4: Construct while loops to do the following:
-  a. Query the user for the starting fuel level. Validate that the user enters a positive, integer value greater than 5000 but less than 30000. */
-
-
-
-
-
-//b. Use a second loop to query the user for the number of astronauts (up to a maximum of 7). Validate the entry.
+  //b. Use a second loop to query the user for the number of astronauts (up to a maximum of 7). Validate the entry.
   
-  
-  
-  
-//c. Use a final loop to monitor the fuel status and the altitude of the shuttle. Each iteration, decrease the fuel level by 100 units for each astronaut aboard. Also, increase the altitude by 50 kilometers.
+while (numAstronauts < 1 || numAstronauts > 7) {
+  numAstronauts = input.question("Enter the number of astronauts: ");
+}
+    
+  //c. Use a final loop to monitor the fuel status and the altitude of the shuttle. Each iteration, decrease the fuel level by 100 units for each astronaut aboard. Also, increase the altitude by 50 kilometers.
+
+  while (fuelStart - (100*numAstronauts) >= 0) {
+    shuttleAlt = shuttleAlt + 50;
+    fuelStart = fuelStart - (100*numAstronauts);
+  }
 
 
+//Exercise #5: Output the result with the phrase, “The shuttle gained an altitude of ___ km.”
 
-/*Exercise #5: Output the result with the phrase, “The shuttle gained an altitude of ___ km.”
+console.log(`The shuttle gained an altitude of ${shuttleAlt} km.`); 
 
-If the altitude is 2000 km or higher, add “Orbit achieved!” Otherwise add, “Failed to reach orbit.”*/
+
+//If the altitude is 2000 km or higher, add “Orbit achieved!” Otherwise add, “Failed to reach orbit.”
+
+if (shuttleAlt >= 2000) {
+  console.log("Orbit achieved!");
+} else if (shuttleAlt < 2000) {
+  console.log("Failed to reach orbit.");
+}
