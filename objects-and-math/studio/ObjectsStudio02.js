959c7204db1cfc812e1c4c88dd9c5e0301bb0a3e
r-vaughn <rebecca.a.vaughn@outlook.com>
2024-02-04 23:20:41 -0600

    Oxygen Depleted

--- objects-and-math/studio/ObjectsStudio02.js
+++ objects-and-math/studio/ObjectsStudio02.js
@@ -1,29 +1,46 @@
 // Code your orbitCircumference function here:
 
 function  orbitCircumference(radius) {
-    let circumference = Math.PI * 2 * radius
-    return circumference
+    let circumference = 2*Math.PI*radius
+    return Math.round(circumference);
 }
 console.log(orbitCircumference(6));
 console.log(orbitCircumference(4));
 
 // Code your missionDuration function here:
 
+//const orbitalRadius = 2000
+//const orbitalSpeed = 28000
 
-function missionDuration(orbitsCompleted) {
-    const orbitalRadius = 5;
-    const orbitalSpeed = 28000;
-    let time = ((orbitCircumference(orbitalRadius))/orbitalSpeed)*orbitsCompleted; 
-    return time
-
+function missionDuration(orbitsCompleted, orbitalRadius = 2000, orbitalSpeed = 28000) {
+  let totalDistance = orbitsCompleted * orbitCircumference(orbitalRadius);  
+  let time = Math.round((totalDistance/orbitalSpeed) * 100) / 100; 
+  return (`The mission will travel ${totalDistance} km around the planet, and it will take ${time} hours to complete.`);
 }
-console.log(missionDuration(5));
+
+console.log(missionDuration(5)  + "\n");
 
 // Copy/paste your selectRandomEntry function here:
 
+function selectRandomEntry(idNumbers) {
+  let randomEntryIndex = Math.floor(Math.random()*idNumbers.length); 
+  return idNumbers[randomEntryIndex];
+}
 
 // Code your oxygenExpended function here:
 
+//function oxygenExpended(candidateObject, orbitsCompleted, orbitalRadius, orbitalSpeed) {
+//  let totalTime = missionDuration(orbitsCompleted, orbitalRadius, orbitalSpeed); 
+//  let oxygenUsed = Math.round(candidateObject.o2Used(totalTime) * 1000) / 1000; 
+//  return `${candidateObject['name']} will perform the spacewalk, which will last ${totalTime} hours and require ${oxygenUsed} kg of oxygen.`; 
+//}
+//
+//console.log(oxygenExpended(candidateA, 5, 2000, 28000));
+//console.log(oxygenExpended(candidateB, 5, 2000, 28000));
+//console.log(oxygenExpended(candidateC, 5, 2000, 28000));
+//console.log(oxygenExpended(candidateD, 5, 2000, 28000));
+//console.log(oxygenExpended(candidateE, 5, 2000, 28000));
+//console.log(oxygenExpended(candidateF, 5, 2000, 28000));
 
 // Candidate data & crew array.
 let candidateA = {
@@ -71,3 +88,15 @@ let candidateA = {
  
  let crew = [candidateA,candidateC,candidateE];
  
+ function oxygenExpended(candidateObject, orbitsCompleted, orbitalRadius, orbitalSpeed) {
+  let totalTime = missionDuration(orbitsCompleted, orbitalRadius, orbitalSpeed); 
+  let oxygenUsed = Math.round(candidateObject.o2Used(totalTime) * 1000) / 1000; 
+  return `${candidateObject['name']} will perform the spacewalk, which will last ${totalTime} hours and require ${oxygenUsed} kg of oxygen.`; 
+}
+
+console.log(oxygenExpended(candidateA, 3, 2000, 28000) + "\n");
+//console.log(oxygenExpended(candidateB) + "\n");
+//console.log(oxygenExpended(candidateC) + "\n");
+//console.log(oxygenExpended(candidateE) + "\n");
+//console.log(oxygenExpended(candidateF) + "\n");
+//console.log(oxygenExpended(candidateD) + "\n");
