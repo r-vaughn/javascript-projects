e39e842ebc7903cc53b2ffa32a5761db5cb654bb
r-vaughn <rebecca.a.vaughn@outlook.com>
2024-02-11 19:46:33 -0600

    Average Astronaut Scores

--- modules/exercises/randomSelect.js
+++ modules/exercises/randomSelect.js
@@ -1,5 +1,13 @@
+const { randomArrayElement } = require("../exporting-modules/practiceExports");
+
 function randomFromArray(arr){
   //Your code here to select a random element from the array passed to the function.
+  let randomElement = Math.floor(Math.random()*arr.length);
+  return arr[randomElement];
 }
+// let animals = ["cat", "dog", "fish", "ferret", "bird"]
+// console.log(randomFromArray(animals));
 
 //TODO: Export the randomFromArray function.
+
+module.exports = randomFromArray; 
