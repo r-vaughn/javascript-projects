41912e59deb2b238ab5c1a7547253c5f74541b7d
r-vaughn <rebecca.a.vaughn@outlook.com>
2024-02-25 19:37:22 -0600

    Flight Simulator updated with abort mission prompt

--- dom-and-events/exercises/script.js
+++ dom-and-events/exercises/script.js
@@ -5,6 +5,27 @@ function init () {
 
     // Put your code for the exercises here.
     
+    button.addEventListener("click", Event => {
+        paragraph.innerHTML = "Houston, we have liftoff!"
+    }); 
+
+    missionAbort.addEventListener("mouseover", Event => {
+        Event.target.style.backgroundColor = "red";
+    });
+
+    missionAbort.addEventListener("mouseout", Event => {
+        Event.target.style.backgroundColor = "";
+    });
+
+    missionAbort.addEventListener("click", Event => {
+        let text;
+        if (confirm("Are you sure you want to abort the mission?") == true) {
+           paragraph.innerHTML = "Mission aborted! Space shuttle returning home."; 
+           setTimeout(function(){
+            location.reload();
+           }, 5000);
+        }
+    });
 }
 
 window.addEventListener("load", init);
