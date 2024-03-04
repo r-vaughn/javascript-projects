eecaa1fe2fc4f34eea5b2b9442d70ba72683130b
r-vaughn <rebecca.a.vaughn@outlook.com>
2024-03-03 19:26:26 -0600

    Flight Simulator working

--- dom-and-events/studio/scripts.js
+++ dom-and-events/studio/scripts.js
@@ -1,2 +1,49 @@
 // Write your JavaScript code here.
 // Remember to pay attention to page loading!
+
+function init () {
+
+    const flightStatus = document.getElementById("flightStatus");
+    const flightDisplay = document.getElementById("flightDisplay");
+    const shuttleBackground = document.getElementById("shuttleBackground");
+    const upButton = document.getElementById("up");
+    const downButton = document.getElementById("down");
+    const rightButton = document.getElementById("right");
+    const leftButton = document.getElementById("left");
+    const spaceShuttleHeight = document.getElementById("spaceShuttleHeight"); 
+    const takeOff = document.getElementById("takeoff"); 
+    const landingButton = document.getElementById("landing"); 
+    const missionAbort = document.getElementById("missionAbort");
+    const rocket = document.getElementById("rocket");
+
+
+    takeOff.addEventListener("click", Event => {
+        if (confirm("Confirm that the shuttle is ready for takeoff.") == true) {
+           flightStatus.innerHTML = "Shuttle in flight.";
+           shuttleBackground.style.backgroundColor = "blue";
+           spaceShuttleHeight.innerHTML = "10,000"        
+        };
+    });
+
+    landingButton.addEventListener('click', Event => {
+        alert("The shuttle is landing. Landing gear engaged.");
+        flightStatus.innerHTML = "The shuttle has landed."; 
+        shuttleBackground.style.backgroundColor = "blue"; 
+        spaceShuttleHeight.innerHTML = "0";
+    });
+
+    missionAbort.addEventListener('click', Event => {
+        if (confirm("Confirm you want to abort the mission.") === true) {
+            flightStatus.innerHTML = "Mission aborted.";
+            shuttleBackground.style.backgroundColor = "green"; 
+            spaceShuttleHeight.innerHTML = '0';
+        };
+    });
+
+    upButton.addEventListener('click', Event => {
+        
+
+    })
+} 
+
+window.addEventListener("load", init);
