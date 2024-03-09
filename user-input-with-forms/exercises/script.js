7a06f5e9537bd87b0abd89a5a67535a97e2e9930
r-vaughn <rebecca.a.vaughn@outlook.com>
2024-03-09 14:57:28 -0600

    Rocket Simulation Form complete

--- user-input-with-forms/exercises/script.js
+++ user-input-with-forms/exercises/script.js
@@ -1,1 +1,15 @@
 //Code Your Solution Below
+
+let form = document.getElementById("form");
+//let formSubmit = document.getElementById("submit");
+
+
+form.addEventListener("submit", (event) => {
+    let formInput = document.getElementById("form");
+        if (formInput.validity() === false) {
+            event.preventDefault();
+            document.getElementById("log").innerHTML = "Missing inputs. Please complete form.";
+        }     
+});
+
+//BONUS: Use an event handler and the submit event to validate that all inputs have values. Do NOT let the form be submitted if inputs are
