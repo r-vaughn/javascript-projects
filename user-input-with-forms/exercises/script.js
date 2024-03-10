6bbe6f8014a4c334e906099767c64cf4fb1d3f1d
r-vaughn <rebecca.a.vaughn@outlook.com>
2024-03-10 00:04:54 -0600

    Search Engine complete

--- user-input-with-forms/exercises/script.js
+++ user-input-with-forms/exercises/script.js
@@ -12,22 +12,22 @@ let button = document.getElementById("submitForm");
 
 window.addEventListener("load", function() {
     form.addEventListener(("submit"), (event) => {
-    let testNameInput = document.querySelector("input[name=testName]");
-    let testDateInput = document.querySelector("input[name=testDate]");
-    let rocketTypeInput = document.querySelector("select[name=rocketType]"); 
-    let boosterCountInput = document.querySelector("input[name=boosterCount]");
-    let windRatingInput = document.querySelector("input[name=windRating]");
-    let productionServers = document.querySelector("input[name=productionServers]");
-    console.log(`testNameInput => ${testNameInput.value}`);
-    console.log(`testDateInput => ${testDateInput.value}`);
-    console.log(`rocketTypeInput => ${rocketTypeInput.value}`);
-    console.log(`boosterCountInput => ${boosterCountInput.value}`);
-    console.log(`windRatingInput => ${windRatingInput.value}`);
-    console.log(`productionServers => ${productionServers.value}`);
-    if ( ( testNameInput.value === "" ) || ( testDateInput.value === "" ) || ( rocketTypeInput.value === "" ) || ( boosterCountInput.value === "" ) || ( windRatingInput.required === false) || ( productionServers.checked === false )  ) {
-        alert("All fields are required!");
-        event.preventDefault();
-    }
+        let testNameInput = document.querySelector("input[name=testName]");
+        let testDateInput = document.querySelector("input[name=testDate]");
+        let rocketTypeInput = document.querySelector("select[name=rocketType]"); 
+        let boosterCountInput = document.querySelector("input[name=boosterCount]");
+        let windRatingInput = document.querySelector("input[name=windRating]");
+        let productionServers = document.querySelector("input[name=productionServers]");
+        console.log(`testNameInput => ${testNameInput.value}`);
+        console.log(`testDateInput => ${testDateInput.value}`);
+        console.log(`rocketTypeInput => ${rocketTypeInput.value}`);
+        console.log(`boosterCountInput => ${boosterCountInput.value}`);
+        console.log(`windRatingInput => ${windRatingInput.value}`);
+        console.log(`productionServers => ${productionServers.value}`);
+        if ( ( testNameInput.value === "" ) || ( testDateInput.value === "" ) || ( rocketTypeInput.value === "" ) || ( boosterCountInput.value === "" ) || ( windRatingInput.required === false) || ( productionServers.checked === false )  ) {
+            alert("All fields are required!");
+            event.preventDefault();
+        }
     });
 });
 
