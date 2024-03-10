0fac19e1765f2709afb0101b8c4b9d87e891f475
r-vaughn <rebecca.a.vaughn@outlook.com>
2024-03-09 18:15:41 -0600

    Bonus Mission completed on Rocket Simulation

--- user-input-with-forms/exercises/script.js
+++ user-input-with-forms/exercises/script.js
@@ -1,15 +1,44 @@
 //Code Your Solution Below
 
-let form = document.getElementById("form");
-//let formSubmit = document.getElementById("submit");
+let form = document.querySelector("form");
+let button = document.getElementById("submitForm");
 
 
-form.addEventListener("submit", (event) => {
-    let formInput = document.getElementById("form");
-        if (formInput.validity() === false) {
-            event.preventDefault();
-            document.getElementById("log").innerHTML = "Missing inputs. Please complete form.";
-        }     
+// button.addEventListener("click", function() {
+    // let formInputs = document.querySelectorAll("input, select");
+    // console.log(formInputs.values());
+// });
+
+
+window.addEventListener("load", function() {
+    form.addEventListener(("submit"), (event) => {
+    let testNameInput = document.querySelector("input[name=testName]");
+    let testDateInput = document.querySelector("input[name=testDate]");
+    let rocketTypeInput = document.querySelector("select[name=rocketType]"); 
+    let boosterCountInput = document.querySelector("input[name=boosterCount]");
+    let windRatingInput = document.querySelector("input[name=windRating]");
+    let productionServers = document.querySelector("input[name=productionServers]");
+    console.log(`testNameInput => ${testNameInput.value}`);
+    console.log(`testDateInput => ${testDateInput.value}`);
+    console.log(`rocketTypeInput => ${rocketTypeInput.value}`);
+    console.log(`boosterCountInput => ${boosterCountInput.value}`);
+    console.log(`windRatingInput => ${windRatingInput.value}`);
+    console.log(`productionServers => ${productionServers.value}`);
+    if ( ( testNameInput.value === "" ) || ( testDateInput.value === "" ) || ( rocketTypeInput.value === "" ) || ( boosterCountInput.value === "" ) || ( windRatingInput.required === false) || ( productionServers.checked === false )  ) {
+        alert("All fields are required!");
+        event.preventDefault();
+    }
+    });
 });
+
+// form.addEventListener("submit", (event) => {
+    //let formInput = document.getElementById("form");
+        // if (form.validity() === false) {
+            // event.preventDefault();
+            // document.getElementById("log").innerHTML = "Missing inputs. Please complete form.";
+        // }     
+// });
+
+//onsubmit
 
 //BONUS: Use an event handler and the submit event to validate that all inputs have values. Do NOT let the form be submitted if inputs are
